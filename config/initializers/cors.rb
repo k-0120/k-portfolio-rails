a1294f13af300ea885b4a2e36cce1aef91e4fb42
koumura.takayuki0120 <koumura.takayuki0120@gmail.com>
2025-05-11 18:48:52 +0900

    added articles

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,9 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'  # ← 最初はワイルドカードでOK。運用時は 'http://localhost:3001' にする
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :patch, :put, :delete, :options]
+  end
+end
